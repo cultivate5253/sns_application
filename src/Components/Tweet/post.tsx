8019154f40ee535d688cb70240d305f1777cb192
yoshinori <ynkakizawa@gmail.com>
2023-01-08 19:13:48 +0900

    "third"

--- src/Components/Tweet/post.tsx
+++ src/Components/Tweet/post.tsx
@@ -18,38 +18,38 @@ setImage(e.target.files[0]);
 };
 
 const handleSubmit = async(e) => {
-e.preventDefault();
+  e.preventDefault();
 
-Copy code
-// Tweetを保存する
-const tweetRef = db.collection("tweets").doc();
-const tweet = {
-  text,
-  user: {
-    id: user.uid,
-    name: user.displayName,
-    avatar: user.photoURL,
-  },
-  createdAt: new Date(),
-};
-await tweetRef.set(tweet);
+  
+  // Tweetを保存する
+  const tweetRef = db.collection("tweets").doc();
+  const tweet = {
+    text,
+    user: {
+      id: user.uid,
+      name: user.displayName,
+      avatar: user.photoURL,
+    },
+    createdAt: new Date(),
+  };
+  await tweetRef.set(tweet);
 
-// 画像がある場合はCloud Storageに保存する
-if (image) {
-  const fileRef = storage.ref().child(`tweet/${tweetRef.id}/${image.name}`);
-  await fileRef.put(image);
-  const imageUrl = await fileRef.getDownloadURL();
-  await tweetRef.update({ imageUrl });
-}
-};
+  // 画像がある場合はCloud Storageに保存する
+  if (image) {
+    const fileRef = storage.ref().child(`tweet/${tweetRef.id}/${image.name}`);
+    await fileRef.put(image);
+    const imageUrl = await fileRef.getDownloadURL();
+    await tweetRef.update({ imageUrl });
+  }
+  };
 
-return (
-<Form onSubmit={handleSubmit}>
-<Input type="text" value={text} onChange={handleChangeText} />
-<Input type="file" onChange={handleChangeImage} />
-<Button type="submit">Tweetする</Button>
-</Form>
-);
-};
+  return (
+  <Form onSubmit={handleSubmit}>
+  <Input type="text" value={text} onChange={handleChangeText} />
+  <Input type="file" onChange={handleChangeImage} />
+  <Button type="submit">Tweetする</Button>
+  </Form>
+  );
+  };
 
 export default TweetForm;
