7c8200a5c7d43a0450fc7e2a452247685d5a21c3
yoshinori <ynkakizawa@gmail.com>
2023-01-13 11:46:36 +0900

    0113-2

--- src/Components/Follow/without_firebase_follow2.tsx
+++ src/Components/Follow/without_firebase_follow2.tsx
@@ -1,58 +1,85 @@
 import React from "react";
 import axios from "axios";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 
 interface Props {
-  userId: string;
-  currentUserId: string;
+userId: string;
+currentUserId: string;
 }
 
+const useStyles = makeStyles((theme) => ({ //スタイルを定義
+button: {
+margin: theme.spacing(1),
+},
+}));
+
 const Follow: React.FC<Props> = (props) => {
-  const { userId, currentUserId } = props;
+const classes = useStyles();  //上記で定義したスタイルをコンポーネントから呼び出せるようにする
 
-  const handleFollow = async () => {
-    try {
-      // Send follow request to server
-      const res = await axios.patch("/api/users", {
-        userId,
-        currentUserId,
-        follow: true,
-      });
+const { userId, currentUserId } = props;
 
-      if (res.status === 200) {
-        console.log("Successfully followed user");
-      } else {
-        console.error("Error following user");
-      }
-    } catch (err) {
-      console.error(err);
-    }
-  };
+const handleFollow = async () => {
+try {
+// Send follow request to server
+const res = await axios.patch("/api/users", {
+userId,
+currentUserId,
+follow: true,
+});
 
-  const handleUnfollow = async () => {
-    try {
-      // Send unfollow request to server
-      const res = await axios.patch("/api/users", {
-        userId,
-        currentUserId,
-        unfollow: true,
-      });
 
-      if (res.status === 200) {
-        console.log("Successfully unfollowed user");
-      } else {
-        console.error("Error unfollowing user");
-      }
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  return (
-    <div>
-      <button onClick={handleFollow}>Follow</button>
-      <button onClick={handleUnfollow}>Unfollow</button>
-    </div>
-  );
+  if (res.status === 200) {
+    console.log("Successfully followed user");
+  } else {
+    console.error("Error following user");
+  }
+} catch (err) {
+  console.error(err);
+}
 };
+
+const handleUnfollow = async () => {
+try {
+// Send unfollow request to server
+const res = await axios.patch("/api/users", {
+userId,
+currentUserId,
+unfollow: true,
+});
+
+
+  if (res.status === 200) {
+    console.log("Successfully unfollowed user");
+  } else {
+    console.error("Error unfollowing user");
+  }
+} catch (err) {
+  console.error(err);
+}
+};
+
+return (
+
+  <div>
+    <Button
+      variant="contained"
+      color="primary"
+      className={classes.button}
+      onClick={handleFollow}
+    >
+      Follow
+    </Button>
+    <Button
+      variant="contained"
+      color="secondary"
+      className={classes.button}
+      onClick={handleUnfollow}
+    >
+      Unfollow
+    </Button>
+  </div>
+);
+}
 
 export default Follow;
