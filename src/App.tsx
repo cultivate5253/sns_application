f14611a55d514c11e58ff6c941e6add96ee690ca
yoshinori <ynkakizawa@gmail.com>
2023-01-13 11:54:41 +0900

    0113-4

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import { Follow } from './components/Follow';
-import { Post } from './components/Post';
-import { DM } from './components/DM';
-import { Auth } from './components/Auth';
-import './App.css';
-import { createTheme, ThemeProvider } from "@material-ui/core/styles";　　//ThemeProviderは、カスタマイズしたスタイルを適用するために使う
+import React from "react";
+import logo from "./logo.svg";
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+import { DM } from "./components/DM";
+import { Search } from "./components/Search";
+import { Post } from "./components/Post";
+import { FavRT } from "./components/FavRT";
+import { Auth } from "./components/Auth";
+import "./App.css";
 
-
-const theme = createTheme({   //独自のスタイルを設定
-  // 追加のCSSスタイル
+const theme = createTheme({
+  // additional CSS styles
 });
 
 function App() {
   return (
-  <ThemeProvider theme={theme}>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-      <Follow />
-      <Post />
-      <DM />
-      
-      <Auth />
-  
-    </div>
-  </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+        </header>
+        <DM />
+        <Search />
+        <Post />
+        <FavRT />
+        <Auth />
+      </div>
+    </ThemeProvider>
   );
 }
 
